7b26ff4ee12c3cb9eefb2c94740e38095ce52184
Nate Miller <brickbuilder9@gmail.com>
2023-02-03 01:13:17 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,74 @@
 #include <iostream>
+#include <vector>
 #include <boost/asio.hpp>
+#include <boost/algorithm/string.hpp>
+
+#ifdef _WIN32
+#include <windows.h>
+#include <tchar.h>
+
+std::vector<std::string> getSerialPorts() {
+  std::vector<std::string> serialPorts;
+
+  TCHAR lpTargetPath[5000];
+  DWORD test = QueryDosDevice(NULL, lpTargetPath, 5000);
+  if (test != 0) {
+    for (TCHAR *p = lpTargetPath; *p; p += _tcslen(p) + 1) {
+      if (_tcsstr(p, _T("COM")) != NULL) {
+        serialPorts.push_back(std::string(p));
+      }
+    }
+  }
+
+  return serialPorts;
+}
+#elif __linux__
+std::vector<std::string> getSerialPorts() {
+  std::vector<std::string> serialPorts;
+
+    try {
+  boost::asio::io_service io;
+  boost::asio::serial_port device(io, "/dev/ttyACM0");
+
+  if (device.is_open()) {
+    serialPorts.push_back("/dev/ttyACM0");
+  }
+    } catch (const std::exception &e) {
+    std::cout << "Error: " << e.what() << std::endl;
+  }
+  return serialPorts;
+}
+#else
+std::vector<std::string> getSerialPorts() {
+  std::vector<std::string> serialPorts;
+  return serialPorts;
+}
+#endif
 
 int main() {
-  // Create a serial port object using the default constructor
+  std::vector<std::string> ports = getSerialPorts();
+  if (ports.empty()) {
+    std::cout << "No serial ports found." << std::endl;
+    return 0;
+  }
+
   boost::asio::io_service io;
-  boost::asio::serial_port port(io);
+  boost::asio::serial_port port(io, ports[0]);
+  port.set_option(boost::asio::serial_port_base::baud_rate(9600));
 
-  // Open the serial port
-  port.open("/dev/ttyACM0");
+  try {
+    for (;;) {
+      boost::asio::streambuf buf;
+      boost::asio::read_until(port, buf, '\n');
 
-  // Set the baud rate, data bits, and other properties of the serial connection
-  port.set_option(boost::asio::serial_port_base::baud_rate(115200));
-  port.set_option(boost::asio::serial_port_base::character_size(8));
-  port.set_option(boost::asio::serial_port_base::parity(boost::asio::serial_port_base::parity::none));
-  port.set_option(boost::asio::serial_port_base::stop_bits(boost::asio::serial_port_base::stop_bits::one));
-  port.set_option(boost::asio::serial_port_base::flow_control(boost::asio::serial_port_base::flow_control::none));
+      std::string line;
+      std::istream(&buf) >> line;
 
-  // Read data from the serial port
-  boost::asio::streambuf buf;
-  boost::asio::read_until(port, buf, '\n');
-
-  // Convert the data to a string and print it
-  std::string data = boost::asio::buffer_cast<const char*>(buf.data());
-  std::cout << data << std::endl;
-
-  // Close the serial port
-  port.close();
+      std::cout << line << std::endl;
+    }
+  } catch (const std::exception &e) {
+    std::cout << "Error: " << e.what() << std::endl;
+  }
 
   return 0;
 }
-

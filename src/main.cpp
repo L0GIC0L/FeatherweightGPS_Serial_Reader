02452334b13101af5fe371788c03d4ced56eb347
Nate Miller <brickbuilder9@gmail.com>
2023-02-09 23:59:42 -0500

    Changes to be committed: 	modified:   src/gui_test.cpp 	deleted:    src/gui_test.o 	deleted:    src/imgui.o 	deleted:    src/imgui_demo.o 	deleted:    src/imgui_draw.o 	deleted:    src/imgui_impl_glfw.o 	deleted:    src/imgui_impl_opengl3.o 	deleted:    src/imgui_tables.o 	deleted:    src/imgui_widgets.o 	deleted:    src/implot.o 	deleted:    src/implot_items.o 	modified:   src/main.cpp 	new file:   src/serial.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,84 +1,52 @@
 #include <iostream>
-#include <vector>
 #include <cstring>
-#include <boost/asio.hpp>
+#include "serial.h"
+
+int main(int argc, char* argv[])
+{
+    ////Normally this can be used to assign a port when launching the program
+    // if (argc < 2)
+    // {
+    //     cerr << "Usage: serialport_example <serial port name>" << endl;
+    //     return 1;
+    // }
+
+    //Change this line to configure the port
+    if (!openSerialPort("COM3"))
+    {
+        return 1;
+    }
 
 #ifdef _WIN32
-#include <windows.h>
-#include <tchar.h>
-
-std::vector<std::string> getSerialPorts() {
-  std::vector<std::string> serialPorts;
-
-  TCHAR lpTargetPath[5000];
-  DWORD test = QueryDosDevice(NULL, lpTargetPath, 5000);
-  if (test != 0) {
-    for (TCHAR *p = lpTargetPath; *p; p += _tcslen(p) + 1) {
-      if (_tcsstr(p, _T("COM")) != NULL) {
-        serialPorts.push_back(std::string(p));
-      }
-    }
-  }
-
-  return serialPorts;
-}
-
-#elif __linux__
-std::vector<std::string> getSerialPorts() {
-  std::vector<std::string> serialPorts;
-
-    try {
-        boost::asio::io_service io;
-        boost::asio::serial_port device(io, "/dev/serial/by-id/usb-FTDI_FT230X_Basic_UART_D201170B-if00-port0");
-
-        if (device.is_open()) {
-            serialPorts.push_back("/dev/serial/by-id/usb-FTDI_FT230X_Basic_UART_D201170B-if00-port0");
+    char incomingData[1] = "";
+    string line = "";
+    DWORD bytesRead;
+    while (incomingData[0] != '\n')
+    {
+        if (ReadFile(hSerial, incomingData, 1, &bytesRead, NULL))
+        {
+            line += incomingData[0];
         }
 
-    } catch (const std::exception &e) {
-    std::cout << "Error: " << e.what() << std::endl;
-  }
-
-  return serialPorts;
-}
+    }
+    cout << "Behold the completed line: " << line << endl;
 
 #else
-std::vector<std::string> getSerialPorts() {
-  std::vector<std::string> serialPorts;
-  return serialPorts;
-}
-#endif
-
-int main() {
-  std::vector<std::string> ports = getSerialPorts();
-  if (ports.empty()) {
-    std::cout << "No serial ports found." << std::endl;
-    return 0;
-  }
-
-  boost::asio::io_service io;
-  boost::asio::serial_port port(io, ports[0]);
-
-  // Set the baud rate, data bits, and stop bits
-  port.set_option(boost::asio::serial_port_base::baud_rate(115200));
-  port.set_option(boost::asio::serial_port_base::stop_bits(boost::asio::serial_port_base::stop_bits::one));
-  port.set_option(boost::asio::serial_port_base::parity(boost::asio::serial_port_base::parity::none));
-
-
-  try {
-    for (;;) {
-      char c = 0;
-      boost::asio::read(port, boost::asio::buffer(&c, 1));
-
-      std::cout << c;
-      if (c == '\n') {
-        std::cout << std::endl;
-      }
+    char incomingData[1] = "";
+    int bytesRead;
+    string line = "";
+    while (incomingData[0] != '\n')
+    {
+        bytesRead = read(serialPort, incomingData, 1);
+        if (bytesRead > 0)
+        {
+            if (incomingData[0] != '\n') {
+                line += incomingData[0];
+            }
+        }
     }
-  } catch (const std::exception &e) {
-    std::cout << "Error: " << e.what() << std::endl;
-  }
-
-  return 0;
+    cout << "Behold the completed line: " << line << endl;
+#endif
+    closeSerialPort();
+    return 0;
 }
-
